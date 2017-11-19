18dd640c4e03a34eb3c8459835a1869a066d5379
Ignacio Fontalvo <ignacio.fontalvo@globant.com>
2017-11-19 00:46:01 -0500

    setup angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { GameComponent } from './game/game.component';
+import { RegisterComponent } from './register/register.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    GameComponent,
+    RegisterComponent,
+    RankingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cbr-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.scss']
+})
+export class RankingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
